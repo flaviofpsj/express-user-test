3c3670575d599ae2a1968149d4199a611339c002
Flavio Farias <ffpsj@ic.ufal.br>
2018-06-27 01:06:54 -0300

    create index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')();
+const bodyParser = require("body-parser");
+
+var cors = require('cors')
+express.use(cors())
+express.use(bodyParser.json());
+express.use(bodyParser.urlencoded({ extended: true }));
+
+//propriedades do modelo users
+var users = [
+	{
+		name: 'Flavio Farias',
+		email: 'ffpsjr@gmail.com',
+		password: 'Vaca505@',
+	}
+];
+
+var nextId = 0;
+express.get('/users', (req, res) => {
+	res.status(200).json({
+		msg:'oi'
+	});
+})
+
+express.get('/users/:id', (req, res) => {
+	console.log(users[req.params.id]);
+	res.status(200).json(users[req.params.id]);
+})
+
+express.post("/users", (req, res) => {
+	const user = {
+		email: req.body.email,
+		password: req.body.password,
+		id: nextId,
+	};
+	nextId = nextId + 1;
+	users.push(user);
+	console.log(user);
+	res.status(201).json(user);
+});
+
+//função post
+express.post("/users/email", (req, res) => {
+	const user = {
+		email: req.body.email,
+		password: req.body.password,
+	};
+
+	console.log(user);
+	for (var i = 0; i < users.length; i++) {
+		if(users[i].email === user.email && users[i].password === user.password){
+			return res.status(200).json({
+				status: 'ok'
+			});
+		}
+	}
+
+	return res.status(200).json({
+		status: 'erro'
+	});
+});
+
+express.listen(8080, () => {
+	console.log('listening at 8080');
+})
